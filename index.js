76fe4ddfe0f4ac6178cc5b3c484527f90d645925
underwoodjo <132414633+underwoodjo@users.noreply.github.com>
2023-11-15 17:14:51 -0500

    added mysql connection

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+// Import node.js, mysql, fs
 const express = require('express');
 const mysql = require('mysql2');
 const fs = require('fs');
 
+// create port
 const port = process.env.PORT || 3001
 const app = express();
+
+// Connect to mySQL database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: 'MySQLpassword',
+      database: 'employeeTracker_db'
+    },
+    console.log(`Connected to the employeeTracker_db database.`)
+  );
