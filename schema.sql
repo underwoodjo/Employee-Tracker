8b355c5afe7c48628bcc33caccf5e111bed10264
underwoodjo <132414633+underwoodjo@users.noreply.github.com>
2023-11-15 17:30:33 -0500

    created tables

---
+++ schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_id INT,
+    employee TEXT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
